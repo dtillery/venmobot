cbbd38a64f4d02df5014ce69c17d8f84b76af483
David Tillery <dtillery@2u.com>
2015-12-25 18:13:39 -0500

    fix result backend for celery

--- venmobot/celeryconfig.py
+++ venmobot/celeryconfig.py
@@ -4,8 +4,6 @@ redis_url = os.environ.get("REDIS_URL")
 
 CELERY_IMPORTS = ("venmobot.tasks")
 
-CELERY_RESULT_BACKEND = "redis"
-REDIS_URL = redis_url
+CELERY_RESULT_BACKEND = redis_url
 
-BROKER_BACKEND = "redis"
 BROKER_URL = redis_url
