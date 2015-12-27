e94cd7d8aee2179f0d9277d291f43819e1d5a933
David Tillery <dtillery@2u.com>
2015-12-27 13:22:35 -0500

    add base_uri property

--- venmobot/handlers/__init__.py
+++ venmobot/handlers/__init__.py
@@ -12,3 +12,7 @@ class BaseHandler(RequestHandler):
             for arg in self.request.arguments:
                 self._arguments[arg] = self.get_argument(arg)
         return self._arguments
+
+    @property
+    def base_uri(self):
+        return "%s//%s" % (self.request.protocol, self.request.host)
