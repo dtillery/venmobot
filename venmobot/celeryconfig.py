f7c4090a3a74bad97e0422aab8bd4c3a4cd5cb1c
David Tillery <dtillery@2u.com>
2016-01-13 22:35:20 -0500

    disable celery broker connection pooling

--- venmobot/celeryconfig.py
+++ venmobot/celeryconfig.py
@@ -4,6 +4,7 @@ redis_url = os.environ.get("REDIS_URL")
 
 CELERY_IMPORTS = ("venmobot.tasks")
 BROKER_URL = redis_url
+BROKER_POOL_LIMIT = 0
 CELERY_TASK_SERIALIZER = 'json'
 
 CELERY_RESULT_BACKEND = redis_url
